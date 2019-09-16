681f1feea04b514d504cadded347a56374e0bb13
Qing Zhen <zhens100mb@mymts.net>
2019-09-16 09:55:22 -0500

    First commit for lphwExamples!

---
+++ ex3.py
@@ -0,0 +1,28 @@
+# Printing prompt for user
+print("I will now count my chickens:")
+
+# do 30 / 6 = 5 first, then 25 + 5 = 30
+print("Hens", 25 + 30 / 6)
+# do 25 * 3 = 75, then 75 % 4 = 3, then 100 - 3 = 97.
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+# do 4 % 2 = 0, then 1 / 4 = 0.25, 
+# then 3 + 2 + 1 - 5 + 0 - 0.25 + 6 = 6.75.
+print(3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# do 3 + 2 = 5, then 5 - 7 = -2, then 5 < -2 is false
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ ex5.py
@@ -0,0 +1,21 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I got {total}.")
+# in vi editor use: %s/beforeWords/afterWords/g  
+# this is to use subsititution %s for the words pattern: beforeWords, change them into: afterWords, g : for the whole file
+# example: %s/my_//g means: change my_ with nothing for the whole file
